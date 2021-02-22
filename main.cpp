d689880a2d53b22b9a62cb17bbcf84844d2e7184
Alex <frstfng@gmail.com>
2021-02-22 19:15:04 +0300

    Complete all quests

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 #define CHECK_VARIABLE(a, b) (cout << (a >= 0 && a < b ? "true" : "false") << endl)
 #define ARRAY_ELEMENT_ACCESS(arr, i, j) (cout << *(*(arr + i) + j) << endl)
-
+#define ARRAY_ELEMENTS_COUNT(arr, type) (cout << (sizeof (arr) / sizeof (int)) << endl)
 
 int main()
 {
@@ -15,7 +15,6 @@ int main()
 //02. Доступ к элементу двумерного массива
   int i = 3, j = 3;
   int array[i][j];
-  int element = 0;
 
   array[0][0] = 54;
   array[0][1] = 55;
@@ -25,6 +24,11 @@ int main()
   array[2][2] = 70;
 
   ARRAY_ELEMENT_ACCESS(array, 0, 1);
+
+//03. Вывод количества элементов массива
+  int arr[7];
+
+  ARRAY_ELEMENTS_COUNT(arr, int);
 
   return 0;
 }
